5f7ad42487f231186ffa4de1da666c2d685e5fa2
Chris Aporta <christopher.aporta@flatironschool.com>
2015-08-24 01:10:18 -0400

    refactor method

--- prime.rb
+++ prime.rb
@@ -1,6 +1,5 @@
 def prime?(n)
-  factor_range = (2...n).to_a
-  remainders = factor_range.collect{|i| n % i}
+  remainders = (2..Math.sqrt(n).to_i).collect{|i| n % i}
   if remainders.include? 0
     false
   else
